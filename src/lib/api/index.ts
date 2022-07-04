905384f5023f683dc040a096312e1ed148e0c7ca
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-07-04 23:36:16 +0900

    chore : withCredentials 설정

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,28 +1,29 @@
-import Axios, { AxiosError } from "axios";
+import axios from "axios";
+import { AxiosError } from "axios";
 
-export const axiosInstance = Axios.create({
+export const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
   withCredentials: true,
 });
 
-// axiosInstance.interceptors.response.use(
-//   (config) => {
-//     try {
-//       return config;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   },
-//   (error: AxiosError) => {
-//     if (
-//       error.response &&
-//       error.response.status &&
-//       [400, 500].includes(error.response.status)
-//     ) {
-//       alert("잘못된 요청입니다.");
-//       window.location.replace("/");
-//     }
+axiosInstance.interceptors.response.use(
+  (config) => {
+    try {
+      return config;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  (error: AxiosError) => {
+    if (
+      error.response &&
+      error.response.status &&
+      [400, 500].includes(error.response.status)
+    ) {
+      alert("잘못된 요청입니다.");
+      window.location.replace("/");
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
