2b8b85cbad24a0749484cd43a9f4129e3274c6a0
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-07-04 22:29:30 +0900

    chore : axios interceptor 에러처리 제거

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -5,24 +5,24 @@ export const axiosInstance = Axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(
-  (config) => {
-    try {
-      return config;
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  (error: AxiosError) => {
-    if (
-      error.response &&
-      error.response.status &&
-      [400, 500].includes(error.response.status)
-    ) {
-      alert("잘못된 요청입니다.");
-      window.location.replace("/");
-    }
+// axiosInstance.interceptors.response.use(
+//   (config) => {
+//     try {
+//       return config;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   },
+//   (error: AxiosError) => {
+//     if (
+//       error.response &&
+//       error.response.status &&
+//       [400, 500].includes(error.response.status)
+//     ) {
+//       alert("잘못된 요청입니다.");
+//       window.location.replace("/");
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
